03bb91f0af905a316a1303f8f86dff822ba26959
duarte.jr105 <duarte.jr105@gmail.com>
2018-10-14 09:50:37 -0300

    update 13/10/18

--- corr_med.py
+++ corr_med.py
@@ -1,6 +1,13 @@
 import numpy as np
 
-def rv(fcst, hist, obs):
+def method(fcst, hist_mean, hist_std, obs_mean, obs_std, m=0):
+    if m == 1:
+        return obs_mean + (fcst-hist_mean)*(obs_std/hist_std)
+    if m == 2:
+        return obs_mean + (fcst-hist_mean)*(obs_mean/hist_mean)
+
+
+def rv(fcst, hist, obs, m=1):
     hist = np.array(hist)
     obs = np.array(obs)
     fcst = np.array(fcst)
@@ -17,7 +24,6 @@ def rv(fcst, hist, obs):
     obs = np.reshape(obs, (len(obs)//12, 12))
     fcst = np.reshape(fcst, (len(fcst)//12, 12))
     
-    
     hist_mean = np.nanmean(hist, 0)
     hist_std = np.nanstd(hist, 0)
     obs_mean = np.nanmean(obs, 0)
@@ -25,7 +31,9 @@ def rv(fcst, hist, obs):
     
     for i in range(len(fcst)):
         for j in range(len(fcst[0])):
-            fcst[i,j] = ((fcst[i,j] - hist_mean[j])/hist_std[j])*obs_std[j] + obs_mean[j]
+            fcst[i,j] = method(fcst[i,j], hist_mean[j], hist_std[j], obs_mean[j],
+                               obs_std[j], m=m)
+            #fcst[i,j] = ((fcst[i,j] - hist_mean[j])/hist_std[j])*obs_std[j] + obs_mean[j]
             
             if fcst[i, j] < 0:
                 fcst[i,j] = 0
