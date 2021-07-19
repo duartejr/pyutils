2c6b629b5135e852307161b590a9da9bb6291231
Duarte Junior <duarte.jr105@gmail.com>
2021-07-19 08:06:42 -0300

    Add files via upload

--- read_nc.py
+++ read_nc.py
@@ -14,10 +14,17 @@ import geopandas as gpd
 
 
 def __in_poly(shp, lat, lon, buffer=0):
-    gpd_shp = gpd.read_file(shp)['geometry'].buffer(buffer)
+    gpd_shp = gpd.read_file(shp)['geometry']
+    gpd_shp = gpd_shp.buffer(buffer)
     bounds = gpd_shp.bounds
-    pos_lon = np.where((lon>=bounds.minx[0]) & (lon<=bounds.maxx[0]))[0]
-    pos_lat = np.where((lat>=bounds.miny[0]) & (lat<=bounds.maxy[0]))[0]
+    pos_lon = np.where((lon >= bounds. minx[0]) & (lon <= bounds. maxx[0]))[0]
+    pos_lat = np.where((lat >= bounds. miny[0]) & (lat <= bounds. maxy[0]))[0]
+    buffer += buffer
+    
+    if buffer > 0:
+        while len(pos_lon) == 0 or len(pos_lat) == 0:
+            pos_lon, pos_lat = __in_poly(shp, lat, lon, buffer)
+
     return pos_lon, pos_lat
 
 
@@ -25,7 +32,7 @@ def __read_var(file, var_name, plat, plon, ptime):
     if len(Dataset(file).variables[var_name].shape) == 3:
         var = Dataset(file).variables[var_name][ptime, plat, plon]
     else:
-        var = Dataset(file).variables[var_name][ptime,:, :, plat, plon]
+        var = Dataset(file).variables[var_name][ptime, :, :, plat, plon]
 
     return var
 
@@ -35,8 +42,11 @@ def __dates(time_units, times):
     try:
         init_date = dt.strptime(time_units.split(' ')[-1], '%Y-%m-%d')
     except:
-        init_date = dt.strptime(init_date[-2], '%Y-%m-%d')
-    
+        try:
+            init_date = dt.strptime(init_date[-2], '%Y-%m-%d')
+        except:
+            init_date = dt.strptime(init_date[-1].split('T')[0], '%Y-%m-%d')
+        
     if time_units.split(' ')[0] == 'months':
         dates = np.array([init_date + relativedelta(months=x) for x in times])
     if time_units.split(' ')[0][:3].lower() == 'day':
@@ -67,15 +77,45 @@ def __get_lat_lon_time(file, variables):
     return lat, lon, time_units, times
 
 
-def read_nc(file, shape=None, var_name='pre', var_time='month', start_date=0,
+def read_nc(file, shape=None, var_name='pre', freq='month', start_date=0,
             end_date=0, buffer=0):
-    
+    '''
+    Parameters
+    ----------
+    file : string
+        NetCDF filename.
+    shape : string, optional
+        Shapefile name.
+    var_name : string, optional
+        variable name. The default is 'pre'.
+    freq : string, optional
+        Time frequency. The default is 'month'.
+    start_date : datetime, optional
+        Initial date. The default is 0.
+    end_date : datetime, optional
+        Final date. The default is 0.
+    buffer : float, optional
+        Buffer value to be applied to shapefile. The default is 0.
+
+    Returns
+    -------
+    var : np.array
+        An np.array with the values of the selected variable.
+    dates: np.array
+        An np.array with the dates in the time interval provided.
+    lat_in: np.array
+        An np.array with the latitudes in the interval provided.
+    lon_in: np.array
+        An np.array with the longitudes in the interval provided.
+
+    '''
+
     variables = [x for x in Dataset(file).variables]
-    
-    if not var_name in variables:
+
+    if var_name not in variables:
         print(f"{var_name} isn't in the {variables}.")
         return
-    
+
     lat, lon, time_units, times = __get_lat_lon_time(file, variables)
 
     if time_units:
@@ -86,7 +126,7 @@ def read_nc(file, shape=None, var_name='pre', var_time='month', start_date=0,
     if start_date and end_date:
         pos_time_in, dates = __dates_in(dates, start_date, end_date)
     else:
-        pos_time_in = range(0,len(dates))
+        pos_time_in = range(0, len(dates))
 
     if shape:
         pos_lon_in, pos_lat_in = __in_poly(shape, lat, lon, buffer)
@@ -97,15 +137,25 @@ def read_nc(file, shape=None, var_name='pre', var_time='month', start_date=0,
         lon_in = lon
         pos_lon_in = range(len(lon))
         pos_lat_in = range(len(lat))
-    
+
     var = __read_var(file, var_name, pos_lat_in, pos_lon_in, pos_time_in)
-    
+
+    try:
+        if freq == 'month' and 'day' in Dataset(file).variables[var_name].units:
+            var *= 30
+    except AttributeError:
+        print(variables)
+        if 'GC' in variables:
+            var *= 30
+        print(AttributeError)
+
     if time_units:
         return var, dates, lat_in, lon_in
     else:
         return np.array(var), lat_in, lon_in
 
-def save_nc(var,lat, lon, dates, filename, time_units, var_name, var_units,
+
+def save_nc(var, lat, lon, dates, filename, time_units, var_name, var_units,
             var_long_name):
     output = Dataset(filename, 'w', format='NETCDF4')
     output.createDimension('time', len(dates))
